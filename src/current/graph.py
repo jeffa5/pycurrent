ae2291922792a4e12e4c87ea12bc0011ad16dbc9
Andrew Jeffery <dev@jeffas.net>
2024-10-15 11:43:47 +0100

    Make graph return string

--- src/current/graph.py
+++ src/current/graph.py
@@ -4,17 +4,16 @@ from collections import deque
 def graph(*args):
     seen = set()
     q = deque(args)
-    print("digraph {")
-    print("rankdir=LR")
+    lines = []
+    lines.append("digraph {")
+    lines.append("rankdir=LR")
     while q:
         node = q.popleft()
         if node in seen:
             continue
 
         for reader in node.readers:
-            print(f"{node.name} -> {reader.name}")
+            lines.append(f"{node.name} -> {reader.name}")
             q.append(reader)
-    print("}")
-
-
-
+    lines.append("}")
+    return "\n".join(lines)
